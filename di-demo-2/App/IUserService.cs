625e3bf4c771defa3cccaeac294860cd9b8266d4
Shalom Levi <shalom.levi@ewave.co.il>
2024-05-23 12:45:48 +0300

    add demo

---
+++ di-demo-2/App/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using Domain;
+
+namespace App
+{
+    public interface IUserService
+    {
+        User GetUser(int id);
+    }
+}
---
+++ di-demo-2/Infra/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+
+namespace Infra
+{
+    public class UserRepository : IUserRepository
+    {
+        public UserRepository()
+        {
+         
+            
+        }
+        public User GetUser(int id)
+        {
+            return new User { Id = id, Name = "John Doe" };
+        }
+    }
+}
